02dad1dcb0c505fa4c1496c51b0458fdba49d0db
Otso Jousimaa <otso@ojousima.net>
2018-11-12 19:29:49 +0200

    3.3.1

--- src/blebroadcast.ts
+++ src/blebroadcast.ts
@@ -6,11 +6,7 @@ export class BleBroadcast {
   public data: BatteryBroadcast | RuuviTagBroadcast;
   public peripheral: Noble.Peripheral;
 
-  public constructor(
-    data: BatteryBroadcast | RuuviTagBroadcast,
-    peripheral: Noble.Peripheral
-    )
-  {
+  public constructor(data: BatteryBroadcast | RuuviTagBroadcast, peripheral: Noble.Peripheral) {
     this.data = data;
     this.peripheral = peripheral;
   }
