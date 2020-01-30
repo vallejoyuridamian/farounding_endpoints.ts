286ff49dd4993d6e48bb8e2a37015c649de8640f
Otso Jousimaa <otso@ojousima.net>
2020-01-30 22:20:37 +0200

    format

--- src/ojousima_endpoint_xx.ts
+++ src/ojousima_endpoint_xx.ts
@@ -4,7 +4,6 @@ const dataFormatStart = 0;
 const dataFormatEnd = dataFormatStart + 1;
 
 export const dfxxparser = (data: Uint8Array): RuuviTagBroadcast => {
-
   const robject: RuuviTagBroadcast = new RuuviTagBroadcast();
   robject.dataFormat = data[dataFormatStart];
 
